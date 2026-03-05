c5427cf613715c3e5ec9cac04b0106a47d54ab45
Adithyan vs <adithyanvshaju@gmail.com>
2026-03-05 15:55:56 +0530

    Time: 0 ms (100.00%) | Memory: 54.3 MB (62.88%) - LeetSync

---
+++ 2235-capitalize-the-title/capitalize-the-title.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} title
+ * @return {string}
+ */
+var capitalizeTitle = function(title) {
+      return title
+        .split(" ")
+        .map(word => {
+            if (word.length <= 2) {
+                return word.toLowerCase();
+            }
+            return word[0].toUpperCase() + word.slice(1).toLowerCase();
+        })
+        .join(" ");
+};
